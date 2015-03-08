eab3b1132ec2536edd964ae01ed32e67d8a46cc7
vengadora <vengadoravg@gmail.com>
2015-03-08 11:54:23 -0400

    ring ring

--- sysadmin.js
+++ sysadmin.js
@@ -1,11 +1,25 @@
 var parseSysadmins = function (level, tilemap, sysadminLayer, phones) {
     var sysadmins = game.add.group();
+    sysadmins.speed = 100;
     tilemap.createFromTiles(1, -1, 'sysadmin', sysadminLayer, sysadmins);
+
+    var update = function () {
+        if (this.phone && this.phone.isRinging()) {
+            this.scale.set(generic.getXDirection(this, this.phone), 1);
+            this.body.velocity.x = this.scale.x * sysadmins.speed;
+            this.animations.play('walk');
+        } else {
+            this.body.velocity.x = 0;
+            this.animations.play('stand');
+        }
+    };
 
     sysadmins.forEach( function (sysadmin) {
         sysadmin.animations.add('walk', [0,1,2,3,4,3,2,1], 8, true);
-        sysadmin.animations.play('walk');
-        //phaser
+        sysadmin.animations.add('stand', [2]);
+        game.physics.arcade.enable(sysadmin);
+        sysadmin.update = update;
+        generic.silentlySetAnchor(sysadmin);
 
         // giving to each sysadmin it's phone
         phones.forEach( function (phone) {
@@ -14,12 +28,6 @@ var parseSysadmins = function (level, tilemap, sysadminLayer, phones) {
                 sysadmin.phone = phone;
             }
         });
-
-        var update = function () {
-            if (this.phone.isRinging()) {
-                // this.scale
-            }
-        };
 
     });
 
